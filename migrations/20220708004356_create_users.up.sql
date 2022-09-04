aedde4a4cd4d544f242b0df1958d396a93f515d7
Vladimir Nesterov <46971653+t67y110v@users.noreply.github.com>
2022-09-04 23:34:27 +0400

    Update 20220708004356_create_users.up.sql

--- migrations/20220708004356_create_users.up.sql
+++ migrations/20220708004356_create_users.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE users (
   id serial not null primary key,
   email varchar not null unique,
-  encrypted_password varchar not null,\
+  encrypted_password varchar not null,
   userName varchar not null,
   seccondName varchar not null,
   isadmin boolean DEFAULT false,
